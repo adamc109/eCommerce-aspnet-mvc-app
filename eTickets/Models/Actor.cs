da12ebcd8ccc3569570ff99901b66192d6a9b11c
Adam <adam1092002@icloud.com>
2023-05-21 21:42:06 +0100

    Added Cinema, Producer & Actor models

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,11 @@
+﻿namespace eTickets.Models
+{
+    public class Actor
+    {
+
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set;}
+        public string Bio { get; set;}
+    }
+}
