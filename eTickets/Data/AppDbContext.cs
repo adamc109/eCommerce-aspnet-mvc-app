a1e23eb2ea042a496110c862d38e506e98d97fb2
Adam <adam1092002@icloud.com>
2023-05-22 23:11:46 +0100

    test1

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -5,6 +5,3 @@ namespace eTickets.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextkknkOptions<AppDbContext> options) : base(options) { } }
-  nmbm
-        hkhk
