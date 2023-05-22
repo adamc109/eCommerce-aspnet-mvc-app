53b3824dd23c74ebdc7cf4dfe12548f4f916b1f4
Adam <adam1092002@icloud.com>
2023-05-22 23:06:43 +0100

    tesr

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -1,8 +1,10 @@
-﻿namespace eTickets.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
 {
     public class Actor
     {
-
+        [Key]
         public int Id { get; set; }
         public string ProfilePictureURL { get; set; }
         public string FullName { get; set;}
